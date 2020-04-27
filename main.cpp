d74429166d51d66c968248920518d73cabea550a
Paul Jones <chickenjones44@gmail.com>
2020-04-27 16:54:41 -0400

    added two function
    I did this to make the code more readable.

--- main.cpp
+++ main.cpp
@@ -6,46 +6,38 @@
 
 using namespace std;
 
-
+bool openFiles(ifstream &inFile, ofstream &outFile);
+void createWordsArray(string line, int &numWords, int charNum, string word);
+//There will always be one word smaller than 16 characters
+//We want to preserve the lines 
 
 int main() {
+    string *words = new string[100];
     ifstream inFile;
     ofstream outFile;
-    string filename;
     string line;
     string word;
-    string words[50];
     int numWords;
     int wordLength;
     int charNum;
+    bool good;
 
-    //open ifstream file
-    cout << "\n\nWhat is the name of the file (ex: filename.txt)\n" << endl << "FILENAME: ";
-    filename = "test.txt";
-    //cin >> filename;
-	inFile.open(filename);
+    good = openFiles(inFile, outFile);
 
-    //open ofstream file
-    cout << "What is the name of the file you want to save to?\n" << endl << "FILENAME: ";
-    filename = "output.txt";
-	//cin >> filename;
-    outFile.open(filename);
-
-    //check if valid
-    if(!inFile){
-        cout<< "Cannot open input file!" << endl;
+    if (good == 0)
         return 1;
-    }
 
     //Where the work happens
     while(!inFile.eof()) {
+        //get next line and reset vars
         getline(inFile, line);
         word = "";
         numWords = 0;
-        charNum = 0;
 
         //remove commas
         line.erase(remove(line.begin(), line.end(), ','), line.end());
+
+        createWordsArray(line, numWords, 0, word, words);
 
         //create array of words
         for (char i : line) {
@@ -72,8 +64,8 @@ int main() {
             if (wordLength > 15) {
                 //If last element
                 if (i == numWords - 1)  {
-                    cout << "Successfully removed " << words[i] << endl;
-                    cout << "     size: " << wordLength;
+                    //cout << "Successfully removed " << words[i] << endl;
+                    //cout << "     size: " << wordLength;
                     numWords--;
                     break;
                 }
@@ -82,19 +74,24 @@ int main() {
                 for (int j = i; j < numWords; j++) 
                     words[j] = words[j+1];
                 
-                cout << "Successfully removed " << words[i] << endl;
-                cout << "     size: " << wordLength;
+                //cout << "Successfully removed " << words[i] << endl;
+                //cout << "     size: " << wordLength;
                 numWords--;
             }
         }
 
         //Write to file
-        for (string i : words) {
-            outFile << i << ", ";
+        for (int i = 0; i < numWords; i++) {
+            if (i == numWords - 2 || numWords == 1) {
+                outFile << words[i];
+                break;
+            }
+
+            outFile << words[i] << ", ";
         }
         outFile << endl;
-        
-        //goto next line
+
+        //goto next line of document
     }
     inFile.close();
     outFile.close();
@@ -102,4 +99,45 @@ int main() {
     cout << "done";
 
     return 0;
+}
+
+bool openFiles(ifstream &inFile, ofstream &outFile) {
+    string filename;
+
+    //open ifstream file
+    cout << "\n\nWhat is the name of the file (ex: filename.txt)\n" << endl << "FILENAME: ";
+    filename = "test.txt";
+    //cin >> filename;
+	inFile.open(filename);
+
+    //open ofstream file
+    cout << "What is the name of the file you want to save to?\n" << endl << "FILENAME: ";
+    filename = "output.txt";
+	//cin >> filename;
+    outFile.open(filename);
+
+    //check if valid
+    if(!inFile){
+        cout<< "Cannot open input file!" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+void createWordsArray(string line, int &numWords, int charNum, string word, string words[]) {
+    for (char i : line) {
+        //check if end of word
+        if (i == ' ' || charNum == line.size()) {
+            words[numWords] = word;
+            word = "";
+            numWords++;
+        }
+
+        //concatanate next char
+        else {
+            word = word + i;
+        }
+        charNum++;
+    }
 }
